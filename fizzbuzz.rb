1ad3a898565c525d920fdfeb830e5632d1f2bd1f
John Fazekas <john.a.fazekas@gmail.com>
2019-10-04 02:00:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(i)
+
+  output = ""
+
+  if(i % 3)
+    output = output + "Fizz"
+  elsif(i % 5)
+    ouput = ouput + "Buzz"
+  end
+
+  puts output
+end
